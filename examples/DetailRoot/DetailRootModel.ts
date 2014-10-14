d869503ab9db26db6e608a16f26dca02245cd0f7
JD Huntington <jdh@microsoft.com>
2014-10-14 15:59:02 -0700

    Detail list example

---
+++ examples/DetailRoot/DetailRootModel.ts
@@ -0,0 +1,30 @@
+import ViewModel = require('../onejs/ViewModel');
+import List = require('../onejs/List');
+import DetailLayout = require('../ListView/DetailLayout');
+
+class DetailRootModel extends ViewModel {
+    items: List;
+    layout: DetailLayout;
+    columns: string[];
+
+    constructor() {
+        super();
+	this.columns = ['index', 'text', 'somethingRandom'];
+	this.layout = new DetailLayout(this.columns); 
+        this.items = new List();
+        for(var i = 0; i < 100; i++) {
+            this.items.push(this.makeObject(i));
+        }
+    }
+
+    makeObject(i: number): any {
+        return {
+	    index: i,
+            text: 'Item ' + i,
+	    somethingRandom: Math.random() * 1000,
+            key: i
+        };
+    }
+}
+
+export = DetailRootModel;
